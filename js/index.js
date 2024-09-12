f1dfb660b285fd854dc0618c57b466fe6661d386
garceteclaudio <garcete.claudio@gmail.com>
2024-09-12 11:31:32 -0300

    se agrego nombre y apellido para mostrar

--- js/index.js
+++ js/index.js
@@ -1,35 +1,35 @@
 function calcularIMC() {
+  let apellido = document.getElementById("apellido").value;
+  let peso = parseFloat(document.getElementById("peso").value);
+  let nombre = document.getElementById("nombre").value;
+  let altura = parseFloat(document.getElementById("altura").value);
 
-    
-    let apellido = document.getElementById("apellido").value;
-    let peso = parseFloat(document.getElementById("peso").value);
-    let nombre = document.getElementById("nombre").value;
-    let altura = parseFloat(document.getElementById("altura").value);
+  var imc = peso / (altura * altura);
 
-    var imc = peso / (altura*altura);
+  let calculo;
 
-    let calculo;
+  if (nombre == "") {
+    calculo = "Campo nombre obligatorio";
+    imc = -999999;
+  } else if (peso < 3 || altura < 0.3) {
+    calculo = "Peso y altura incorrectos";
+    imc = -999999;
+  } else if (imc < 18.5) {
+    calculo = "Estás delgado/a.";
+  } else if (imc >= 18.5 && imc < 24.9) {
+    calculo = "Estás en el peso ideal.";
+  } else if (imc >= 25 && imc < 29.9) {
+    calculo = "Tienes sobrepeso.";
+  } else if (imc > 29.9) {
+    calculo = "Tienes obesidad. Haz dieta.";
+  }
 
-    if(nombre==""){
-        calculo= "Campo nombre obligatorio";
-        imc=-999999;
-    }else if(peso<3 || altura <0.30){
-        calculo = "Peso y altura incorrectos";
-        imc=-999999;
-    }else if (imc < 18.5) {
-        calculo = "Estás delgado/a.";
-    } else if (imc >= 18.5 && imc < 24.9) {
-        calculo = "Estás en el peso ideal.";
-    } else if (imc >= 25 && imc < 29.9) {
-        calculo = "Tienes sobrepeso.";
-    } else if (imc >29.9){
-        calculo = "Tienes obesidad. Haz dieta.";
-    } 
-
-    const resultado = `
+  const resultado = `
+        Nombre y apellido: ${nombre}  ${apellido}
+        <br>
         Tu IMC es: ${imc}
         <br>
         Obeservación: ${calculo}
     `;
-    document.getElementById("resultado").innerHTML = resultado;
+  document.getElementById("resultado").innerHTML = resultado;
 }
